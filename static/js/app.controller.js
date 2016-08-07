0f7460c1cb3e4963c88589cae82cef49ba9c1bb1
Мирослав Гришин <sergiy.1911@gmail.com>
2016-08-07 21:11:51 +0300

    Removed app.controller - moved logic to app.js

--- static/js/app.controller.js
+++
@@ -1,9 +0,0 @@
-// 'use strict';
-
-angular.module('work_app').
-    controller('appController', ['$scope', '$log', function ($scope, $log) {
-        $scope.test_1 = "123456";
-        // $scope.getResults = function() {
-        //     $log.log("test");
-        // }
-    }]);
